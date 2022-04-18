1c6ca06c189f66e13c94a31603f6334b6cf4f0e5
PRiquelmeA <sith.boabhan@gmail.com>
2022-04-18 02:30:42 -0400

    ultimos cambios

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,4 +3,8 @@ $(document).ready(function () {
     $("#mandar").click(function () {
         alert("el correo fue enviado correctamente...");
     })
+    $(".card-title").click(function () {
+        $(".card-textToggle").toggle({
+        });
+    });
 });
